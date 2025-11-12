1c813f24d355ad68de66e9144ff259baae02e1db
Yaman <eliver.com12@gmail.com>
2025-11-12 15:45:39 +0400

    Add catchyos Support

--- boot.sh
+++ boot.sh
@@ -156,7 +156,7 @@ detect_distro() {
 # Initialize package manager based on distro
 init_package_manager() {
     case "$DISTRO" in
-        arch|manjaro|endeavouros|garuda|arcolinux)
+        arch|manjaro|endeavouros|garuda|arcolinux|cachyos)
             PKG_MANAGER="pacman"
             PKG_INSTALL="sudo pacman -S --needed --noconfirm"
             PKG_UPDATE="sudo pacman -Sy"
